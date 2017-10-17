88670a7afba71c97d146d1584f87ce9cd8c3f770
Erick Camacho <erickcamacho1@gmail.com>
2017-10-17 23:46:41 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,44 @@
 def welcome
-  # code #welcome here
+    puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+    rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+    puts "Your cards add up to #{card_total}"
+    card_total
 end
 
 def prompt_user
-  # code #prompt_user here
+    puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+    input = gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+    card_total = deal_card + deal_card
+    display_card_total(card_total)
+
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+    prompt_user
+    if get_user_input == 'h'
+        card_total += deal_card
+    end
+    card_total
 end
 
 def invalid_command
-  # code invalid_command here
+    puts "Invalid"
 end
 
 #####################################################
@@ -39,6 +46,13 @@ end
 #####################################################
 
 def runner
-  # code runner here
+    welcome
+    card_total = initial_round #deals first two cards and adds them together
+
+    while card_total < 21 #if the card_total above is less than 21 then enters this block
+        card_total = hit?(card_total) #new card total becomes the previous total + hit
+        card_total = display_card_total(card_total) #displays new card_total if still under 21 as again if the user wants a hit
+    end
+        end_game(card_total) #if above 21 then tells the user that they are above 21 and ends the game
+
 end
-    
